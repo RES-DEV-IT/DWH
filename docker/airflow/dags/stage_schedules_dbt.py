8cad1b4f369407d6971bb2023ea671608ae684f7
Artem <a.letyagin1@gmail.com>
2025-12-03 10:43:59 +0300

    03.12.2025 - stage schedules dbt model

---
+++ docker/airflow/dags/stage_schedules_dbt.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+
+CRON_EXP = None
+START_DATE = datetime(2025, 11, 27, 4, 0, 0, 0)
+DBT_MODEL_NAME = "stage_schedules"
+
+default_args = {
+    "owner": "Artem",
+    "retries": 0
+}
+
+with DAG(
+    dag_id="stage_schedules",
+    default_args=default_args,
+    start_date=START_DATE,
+    schedule_interval=CRON_EXP,
+    catchup=False
+) as dags:
+    
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command=f"/home/airflow/.local/bin/dbt run -m {DBT_MODEL_NAME}",
+        cwd='/opt/airflow/plugins/dbt_project',
+        env={
+            "DBT_PROFILES_DIR": "."
+        }
+    )
