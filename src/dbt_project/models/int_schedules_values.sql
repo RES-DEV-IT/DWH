cc48b8edb9e2fce690f9bbec887d571e853a08d3
Artem <a.letyagin1@gmail.com>
2025-12-08 17:23:30 +0300

    08.12.2025 - int_schedule_values

--- src/dbt_project/models/int_schedules_values.sql
+++ src/dbt_project/models/int_schedules_values.sql
@@ -27,12 +27,9 @@ from (
 		jsonb_array_elements(content)->>'lot' as lot,
 		jsonb_array_elements(content)->>'po_item' as po_item,
 		jsonb_array_elements(content)->>'size' as size,
-		SPLIT_PART(
-			COALESCE(
-				jsonb_array_elements(content)->>'qty_of_valves',
-				jsonb_array_elements(content)->>'qty_of_pumps'
-			), '.', 1
-		) as qty,
+		SPLIT_PART(jsonb_array_elements(content)->>'qty_of_valves', '.', 1) as qty_of_valves,
+		SPLIT_PART(jsonb_array_elements(content)->>'qty_of_pumps', '.', 1) as qty_of_pumps,
+		
 		LOWER(jsonb_array_elements(content)->>'priority') as priority,
 		LOWER(jsonb_array_elements(content)->>'status') as status,
 		jsonb_array_elements(content)->>'manufacturing_clearance' as manufacturing_clearance
@@ -45,7 +42,7 @@ where {{ is_project_no('project_no') }}
 	and {{ is_positive_integer('qty') }}
 	and {{ is_priority_valid('priority') }}
 	and {{ is_status_valid('status') }}
-
+	and ( {{ is_status_valid('qty_of_valves') }} or {{ is_status_valid('qty_of_pumps') }})
 
 {% if is_incremental() %}
 	and _created_at > (select max(_created_at) from {{ this }})
