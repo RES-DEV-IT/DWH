1af0820aba049e317e993db21ddba23ea597e4ab
Artem <a.letyagin1@gmail.com>
2025-12-19 16:04:35 +0300

    19.12.2025 - grafana

--- src/dbt_project/models/int_schedules_values.sql
+++ src/dbt_project/models/int_schedules_values.sql
@@ -43,7 +43,7 @@ where {{ is_project_no('project_no') }}
 	and {{ is_priority_valid('priority') }}
 	and {{ is_status_valid('status') }}
 	and ( {{ is_positive_integer('qty_of_valves') }} or {{ is_positive_integer('qty_of_pumps') }})
-
+	
 {% if is_incremental() %}
 	and _created_at > (select max(_created_at) from {{ this }})
 {% endif %}
