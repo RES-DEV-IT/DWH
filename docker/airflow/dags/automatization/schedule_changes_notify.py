86a3a36f6c84f36e685a14715a3857778ef56759
Artem <a.letyagin1@gmail.com>
2025-12-23 20:01:26 +0300

    23.12.2025 - chages

---
+++ docker/airflow/dags/automatization/schedule_changes_notify.py
@@ -0,0 +1,37 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import task
+from airflow import DAG
+import datetime
+
+
+@task
+def fetch_changes():
+    hook = PostgresHook(postges_conn_id="resdb_connection")
+
+    conn = hook.get_conn()
+    cursor = conn.cursor()
+
+    records = cursor.get_records("""
+        select *
+        from schedules_changes as s
+        left join project_responsible as pr
+        on s.project = pr.project
+    """)
+
+    for row in records:
+        print(row)
+
+default_args = {
+    "owner": "Artem",
+    "retries": 0
+}
+
+with DAG(
+    dag_id=f"portal_projects",
+    default_args=default_args,
+    start_date=datetime.datetime(2025, 11, 27, 4, 0, 0, 0),
+    schedule_interval="0 0 * * *",
+    catchup=False
+) as dags:
+    fetch_changes()
+    
