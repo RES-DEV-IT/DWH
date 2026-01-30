dd2b33f486f877a6fb356fcb62cc36bdefb53038
Artem <a.letyagin1@gmail.com>
2026-01-30 10:55:46 +0300

    30.01.2026

---
+++ docker/airflow/dags/automatization/icp_metrics/icp_metrics_sheet.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from airflow.decorators import task
+from airflow import DAG
+from pyairtable import Table, Api
+from gspread import service_account
+
+
+SERVICE_ACCOUNT_CREDS_PATH = "./plugins/schedules/download/submitted-tables-download-v02-750e825a7950.json"
+SHHETS_URL = "https://docs.google.com/spreadsheets/d/1Clbm3ie2e8HqLjHu0FLJVMUt51Y_4Fgb8Q8O6WfbpJw0"
+
+def insert_to_gs(data_to_insert, sheet_name):
+    client = service_account(SERVICE_ACCOUNT_CREDS_PATH)
+
+    worksheet = client.open_by_url(SHHETS_URL).worksheet(sheet_name)
+    # client.open_by_url(LEADS_URL).add_worksheet(title="New sheet", rows=100, cols=20)
+
+    row_start = 2
+    col_start = 1
+
+    worksheet.update(
+        f"R{row_start}C{col_start}:R{row_start + len(data_to_insert) - 1}C{col_start + len(data_to_insert[0]) - 1}",
+        data_to_insert,
+        raw=False
+    )
+
+@task
+def main_task():
+    insert_to_gs([[1,2,3], [4, 5, 6]], "Changes")
+
+with DAG(
+    dag_id="icp_metrics_sheet",
+    default_args={"owner": "Artem", "retries": 0},
+    start_date=datetime(2025, 11, 27, 4, 0, 0, 0),
+    schedule_interval=None,
+    catchup=False
+):
+    main_task()
