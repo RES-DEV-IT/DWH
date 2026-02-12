783df8415a663855413689bf4fc552c6ae6b84a6
Artem <a.letyagin1@gmail.com>
2026-02-12 12:36:32 +0300

    11.02.2026 - new account

--- docker/airflow/dags/master_schedule.py
+++ docker/airflow/dags/master_schedule.py
@@ -27,6 +27,7 @@ def main_task():
     # === Получаем актуальные записи ===
     records = hook.get_records("""
         SELECT 
+            _created_at,
             _manuf,
             _sheet_name,
             jsonb_array_elements(content) as content
@@ -36,21 +37,16 @@ def main_task():
         ) as t1
         WHERE _created_at = max_created_at 
           AND _created_at > CURRENT_TIMESTAMP - interval '1 day'
-        LIMIT 10      
+        LIMIT 10   
     """)
 
     # === Получаем все возможные колонки ===
-    unique_columns = set(["_manuf", "_sheet_name"])
+    unique_columns = set(["_created_at", "_manuf", "_sheet_name"])
 
-    # for r in records:
-    #     for k in list(r[2]):#.keys():
-    #         if k != "po_item":
-    #             r[2].pop(k, None)
+    
     for r in records:
         fields_from_pg = r[2].keys()
         fields_from_pg_normalized = [normalize_string(field) for field in fields_from_pg]
-        # print(">>>", fields_from_pg)
-        # print("@@@", fields_from_pg_normalized)
         unique_columns.update(fields_from_pg_normalized)
 
     # === Обновляем колонки в airtable ===
@@ -65,9 +61,18 @@ def main_task():
             # table.create_field(unique_column, field_type="singleLineText")
             table_for_field_create.create_field(unique_column, field_type="multilineText")
 
+    # === Преобразуем данные в формат AirTable ===
+    at_records = []
+    for record in records:
+        at_record = records[-1] # content
+        at_record["_created_at"] = records[0].copy()
+        at_record["_manuf"] = records[1]
+        at_record["_sheet_name"] = records[2]
+        at_records.append(at_record)
 
     # === Добавляем данные ===
-    at_records = [r[2] for r in records[:20]] # extract content from each row
+
+    at_records = [r[2] for r in records] # extract content from each row
     for r in at_records:
         print("KKK", r.keys())
     table.batch_create(at_records, typecast=True)
